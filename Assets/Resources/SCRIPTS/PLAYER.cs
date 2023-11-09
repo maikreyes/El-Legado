8ab8fdcd17af8aa619ad4a2fc51aa15970f4a24b
Checho653 <szapatap653@gmail.com>
2023-11-09 02:08:41 -0500

    Prototipo 1.2
    Se arreglo el salto y se le hicieron cambios al prototipo

--- Assets/Resources/SCRIPTS/PLAYER.cs
+++ Assets/Resources/SCRIPTS/PLAYER.cs
@@ -7,7 +7,10 @@ public class PLAYER : MonoBehaviour
     public GameObject MapaPrincipal;
     public GameObject MapaSecundario;
     public LayerMask capaSuelo;
+    public float velocidad;
+    public float fuerzaSalto;
 
+    private Rigidbody2D rigidbody;
     private BoxCollider2D boxCollider;
     bool canJump;
     float jump;
@@ -16,72 +19,41 @@ public class PLAYER : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        rigidbody = GetComponent<Rigidbody2D>();
         boxCollider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-         if (Input.GetKey("left"))
-         {
-            gameObject.transform.Translate(-10f * Time.deltaTime,0,0);
-         }
-         if (Input.GetKey("right"))
-         {
-            gameObject.transform.Translate(10f * Time.deltaTime,0,0);
-         }
-        if(Input.GetKey("up"))
-        {
-            ManageJump();
-        }
         if(Input.GetKeyDown("x"))
         {
             Teleport();
         }
 
-       
+        ProcesarMovimiento();
 
-       
+        ProcesarSalto();
+    }
+
+    void ProcesarMovimiento()
+    {
+        float inputMovimiento = Input.GetAxis("Horizontal");
+        rigidbody.velocity = new Vector2(inputMovimiento * velocidad, rigidbody.velocity.y);
+    }
+
+    void ProcesarSalto()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow) && EstaEnELSuelo())
+        {
+            rigidbody.AddForce(Vector2.up * fuerzaSalto, ForceMode2D.Impulse);
+        }
     }
 
     bool EstaEnELSuelo()
     {
         RaycastHit2D raycasthit = Physics2D.BoxCast(boxCollider.bounds.center, new Vector2(boxCollider.bounds.size.x, boxCollider.bounds.size.y), 0f, Vector2.down, 0.2f, capaSuelo);
-        return raycasthit != null; 
-    }
-
-    void FixedUpdate()
-    {
-        anteriorposy = gameObject.transform.position.y;
-    }
-    void ManageJump()
-    {
-        if (EstaEnELSuelo())
-        {
-            canJump = true;
-        }
-        else
-        {
-            canJump = false;
-        }
-
-        if (canJump)
-        {
-            jump += 1.5f;
-            jumpSpeed = 2f;
-        }
-
-        float newY = Mathf.Lerp(gameObject.transform.position.y, (gameObject.transform.position.y + jump)*Time.deltaTime, Time.deltaTime * jumpSpeed);
-        gameObject.transform.position = new Vector3(gameObject.transform.position.x, newY, gameObject.transform.position.z);
-
-        if (jump > 0)
-        {
-            jump -= 0.005f + (jump / 50);
-        }
-        else
-        {
-            jump = 0;
-        }
+        return raycasthit.collider != null; 
     }
 
 
